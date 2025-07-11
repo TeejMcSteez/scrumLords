95ae9436558b6efa18ea36626c35ce6b8ea8cba7
TeejMcSteez <tjhall047@gmail.com>
2025-07-11 19:17:21 -0400

    feat: update styles to make more sense

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-const buttonStyles = "m-1 p-1 text-xl rounded-xl bg-slate-500 hover:bg-slate-300";
+const buttonStyles = "m-1 p-3 text-xl rounded-xl bg-slate-500 hover:bg-slate-300";
 
 export default function Navbar() {
     return(
-        <div className="justify-between m-2 p-1">
+        <div className="flex justify-between m-2 p-1">
             <h1 className="m-2 text-2xl">Scrum Lords</h1>
-            <nav>
+            <nav className="p-2">
                 <a href="#about" className={buttonStyles}>About</a>  
                 <a href="#work" className={buttonStyles}>Work</a>   
             </nav>
