ddfebe386cd71d741a8bfa0e8ea16e222db2c343
TeejMcSteez <tjhall047@gmail.com>
2025-07-12 11:05:34 -0400

    feat: added GithubData interface to make types more readable

--- app/components/GithubApi.tsx
+++ app/components/GithubApi.tsx
@@ -1,10 +1,14 @@
+interface GithubData {
+    name: string, 
+    owner: string
+}
 /**
  * Fetches repo project information from the Github API
  * @param name - Name of the repo on Github to fetch 
  * @param owner - Name of the owner of the repo on Github to fetch
  * @returns Promise<Any>
  */
-export async function getGithubData(name: string, owner: string) {
+export async function getGithubData({name, owner}: GithubData) {
     const res = await fetch(`https://api.github.com/repos/${owner}/${name}`, {
         next: {revalidate: 3600}, // Cache for one hour
     });
@@ -19,8 +23,8 @@ export async function getGithubData(name: string, owner: string) {
  * @param owner - Github username of the owner of the repo
  * @returns <div id="work" className="flex flex-col item-center dark:text-white text-2xl"><h2>{data.name}</h2><p>{data.description}</p><p>Language: {data.language}</p><p><a href={data.git_url}>Clone Me!</a></p></div>
  */
-export default async function GithubDataTemplate({owner, name}: {name: string, owner: string}) {
-    const data = await getGithubData(name, owner);
+export default async function GithubDataTemplate({name, owner}: GithubData) {
+    const data = await getGithubData({name, owner});
 
     return (
         <div id="work" className="flex flex-col item-center dark:text-white text-2xl">
