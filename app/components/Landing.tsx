10c6bf7c51d4a23d84e3151ab593e3abc0994974
TeejMcSteez <tjhall047@gmail.com>
2025-07-11 22:20:07 -0400

    feat: added typewriter component to landing page

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -1,16 +1,15 @@
+import Typewriter from "./Typewriter";
+
 export default function Landing() {
+
+    const intro = "This is the group webpage for the Scrum Lords ITSC3155 Final Project Team!"
+    const hero = "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
+
     return(
-        <div id="about">
-            <p className="bg-slate-400 items-center justify-center rounded-xl">
-                Lorem ipsum dolor sit amet consectetur adipiscing elit. 
-                Quisque faucibus ex sapien vitae pellentesque sem placerat. 
-                In id cursus mi pretium tellus duis convallis. 
-                Tempus leo eu aenean sed diam urna tempor. 
-                Pulvinar vivamus fringilla lacus nec metus bibendum egestas. 
-                Iaculis massa nisl malesuada lacinia integer nunc posuere. 
-                Ut hendrerit semper vel class aptent taciti sociosqu.
-                Ad litora torquent per conubia nostra inceptos himenaeos.
-            </p>
-        </div>
+        <>
+            <div id="about" className="mt-10 p-2 flex flex-col items-center justify-center bg-slate-500 rounded-xl">
+                <Typewriter text={intro} speed={75} />
+            </div>
+        </>
     );
 }
