2ba75e06e8953c97a83394d9c05ef95067d6d493
TeejMcSteez <tjhall047@gmail.com>
2025-07-12 00:06:14 -0400

    feat: update template to be more info rich

--- app/components/GithubApi.tsx
+++ app/components/GithubApi.tsx
@@ -12,9 +12,11 @@ export default async function GithubDataTemplate() {
     const data = await getGithubData();
 
     return (
-        <div>
-            <h2>{data.full_name}</h2>
+        <div id="work" className="flex flex-col item-center dark:text-white text-2xl">
+            <h2>{data.name}</h2>
             <p>{data.description}</p>
+            <p>Language: {data.language}</p>
+            <p><a href={data.git_url}>Clone Me!</a></p>
         </div>
     );
 }
